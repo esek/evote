74f922603d752256849a152be73fef5675dd5298
Jonathan Noxet Sönnerup <Nooxet@gmail.com>
2015-10-11 21:26:42 -0400

    init commit, added a simple index file and some ascii art, just bacause *Det var baettre foerr*

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <title>E vote - Ditt digitala röstsystem</title>
+    <meta charset="UTF-8">
+    <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+
+    <section>
+        <form action="">
+            Email <input type="email">
+        </form>
+    </section>
+</body>
+
+</html>

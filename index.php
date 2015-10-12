aa7ec045bf5228273b836264f7b8a738359255b7
Jonathan Noxet Sönnerup <Nooxet@gmail.com>
2015-10-12 06:13:48 -0400

    added bootstrap

--- index.php
+++ index.php
@@ -5,17 +5,28 @@
 <head>
     <title>E vote - Ditt digitala röstsystem</title>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+
     <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
 
-    <nav>
-        <ul>
-            <li><a href="">Home</a>
-            <li><a href="">Admin</a>
-        </ul>
-    </nav>
+    <div class="container">
+        <div class="header clearfix">
+            <nav>
+            <ul class="nav nav-pills pull-right">
+                <li><a href="">Home</a>
+                <li><a href="">Admin</a>
+            </ul>
+            </nav>
+            <h3 class="text-muted">Test project</h3>
+        </div>
 
     <header>
         <pre> <?php include "evote.php"; ?> </pre>
@@ -25,6 +36,8 @@
     <footer>
         <pre> <?php include "footer.php"; ?> </pre>
     </footer>
+
+    </div> <!-- /container -->
 </body>
 
 </html>

5c2957f701974d2d2d173dc99bff0068674597d1
Tomas Nilsson <tomas.nilsson92@gmail.com>
2015-10-20 18:15:42 +0200

    New design (work in progress)

--- index.php
+++ index.php
@@ -11,33 +11,72 @@
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
+    <link href="css/evote.css" rel="stylesheet">
 
     <link href='http://fonts.googleapis.com/css?family=Droid+Sans' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
 
-    <div class="container">
-        <div class="header clearfix">
-            <nav>
-            <ul class="nav nav-pills pull-right">
-                <li><a href="index.php">Home</a>
-                <li><a href="admin.php">Admin</a>
-            </ul>
-            </nav>
+    <!-- Header -->
+    <div class="fixed-header">
+        <div class ="row">
+            <div class="col-md-4">
+                <div class="logo">
+                    <div><h3><span class="label label-info">INSERT LOGO HERE</span></h3></div>
+                </div>
+            </div>
         </div>
 
-    <header>
-        <xmp class="text-center"><?php include "evote.php"; ?></xmp>
-    </header>
 
-    <pre> Test width LOLOL</pre>
+        <div class="navbar navbar-inverse navbar-default" role="navigation">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">
+                        <span>E-vote</span>
+                    </a>
+                </div>
+                <div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown user-dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                                Dropdown<span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li>Insert link here</li>
+                                <li>Logga ut</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>  
+        </div>
+    </div>
 
-    <footer>
-        <xmp class="text-center"><?php include "footer.php"; ?></xmp>
-    </footer>
 
-    </div> <!-- /container -->
+    <!-- Sidebar -->
+    <div class="container-fluid">
+
+        <div class="col-sm-3 sidebar navbar-collapse collapse col-md-2">
+            <ul class="nav nav-sidebar">
+                <li class="nav-header disabled"><a>Menyrubrik</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="admin.php">Admin</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <!-- Main content -->
+    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        INSERT CONTENT HERE
+    </div>
+
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>

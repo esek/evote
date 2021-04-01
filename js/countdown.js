4613a49c40ec9652f15b7750002f3d3ebbd6ca8a
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-04-01 14:32:49 +0200

    Final changes to countdown.js
    cb and cd were really bad variable names, and cb wasn't even needed for
    all I know. This is better.

--- js/countdown.js
+++ js/countdown.js
@@ -6,23 +6,22 @@
  * @param visual If element with id countdown-counter should be decreased
  */
 function createCountdown(callback, visual) {
-    // Memoized current countdown and callback
-    let cd = 5;
-    const cb = callback;
+    // Memoized current countdown
+    let countdownCounter = 5;
 
     // This function is called when outer function is created
     // (starts counting immedietly)
     (function countdown() {
         // If we have finished counting, call function
         // sent by caller (lol)
-        if (cd === 0) {
-            cb();
+        if (countdownCounter === 0) {
+            callback();
         } else {
-            cd = cd - 1;
-            // Set element with id countdown-counter to cd
+            countdownCounter = countdownCounter - 1;
+            // Set element with id countdown-counter to countdownCounter
             // if visual cue
             if (visual) {
-                document.getElementById("countdown-counter").innerHTML = `${cd}`;
+                document.getElementById("countdown-counter").innerHTML = `${countdownCounter}`;
             }
             setTimeout(countdown, 1000);
         }

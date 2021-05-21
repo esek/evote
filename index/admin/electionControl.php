844965a254bb317579f170994f2a98bdcbcec1d6
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-21 10:58:41 +0200

    Adds script to give feedback of created election
    If the admin creates a new election now and choose to receive election
    codes in CSV format, they receive no feedback except for the CSV file.

--- index/admin/electionControl.php
+++ index/admin/electionControl.php
@@ -18,20 +18,32 @@ if(in_array($priv, $access)){
     	<h3>Skapa nytt val</h3>
     	<hr>
     	<div class="well" style="max-width: 400px">
-    	<form action="/actions/electionadminpagehandler.php" method="POST">
-    	<div class="form-group">
-    	        <label for="vn">Namn på val:</label>
-    	        <input type="text" name="valnamn" class="form-control" id="vn" autocomplete="off">
+			<div id="new-election-form">
+				<form action="/actions/electionadminpagehandler.php" method="POST">
+				<div class="form-group">
+						<label for="vn">Namn på val:</label>
+						<input type="text" name="valnamn" class="form-control" id="vn" autocomplete="off">
+				</div>
+				<div class="form-group" style="max-width: 150px">
+						<label for="ap">Max antal personer:</label>
+						<input type="number" name="antal_personer" class="form-control" id="ap" min="1" autocomplete="off">
+				</div>
+				<input type="checkbox" id="csv_checkbox" name="csv_checkbox">
+				<label for="csv_checkbox">Få koder i CSV-format (för distansröstning)</label>
+				<button type="submit" onclick="placeholder()" class="btn btn-primary" value="create" name="button">Skapa</button>
+				</form>
+			</div>
+			<div id="submit-placeholder" style="display: none">
+				<h4>Ett nytt val har skapats!</h4>
+			</div>
     	</div>
-    	<div class="form-group" style="max-width: 150px">
-    	        <label for="ap">Max antal personer:</label>
-    	        <input type="number" name="antal_personer" class="form-control" id="ap" min="1" autocomplete="off">
-    	</div>
-		<input type="checkbox" id="csv_checkbox" name="csv_checkbox">
-		<label for="csv_checkbox">Få koder i CSV-format (för distansröstning)</label>
-    	<button type="submit" class="btn btn-primary" value="create" name="button">Skapa</button>
-    	</form>
-    	</div>
+		<scrip>
+		// Give user feedback of new election
+		function placeholder() {
+			document.getElementById("new-election-form").style.display = "none";
+			document.getElementById("submit-placeholder").style.display = "";
+		}
+		</scrip>
 
     <?php }else{
         ?>

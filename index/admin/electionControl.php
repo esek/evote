ec5c0477e224465d1b0e4e30c1e8ceaa3fc253e9
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 20:48:21 +0200

    electioncontrol fully translated

--- index/admin/electionControl.php
+++ index/admin/electionControl.php
@@ -15,42 +15,42 @@ if(in_array($priv, $access)){
     if(!$ongoingSession){ ?>
 
 
-    	<h3>Skapa nytt val</h3>
+    	<h3><?php echoLanguageChoice("Skapa nytt val", "Create new election")?></h3>
     	<hr>
     	<div class="well" style="max-width: 400px">
     	<form action="/actions/electionadminpagehandler.php" method="POST">
     	<div class="form-group">
-    	        <label for="vn">Namn på val:</label>
+    	        <label for="vn"><?php echoLanguageChoice("Namn på val:", "Name of election:")?></label>
     	        <input type="text" name="valnamn" class="form-control" id="vn" autocomplete="off">
     	</div>
     	<div class="form-group" style="max-width: 150px">
-    	        <label for="ap">Max antal personer:</label>
+    	        <label for="ap"><?php echoLanguageChoice("Max antal personer:", "Max number of people:")?></label>
     	        <input type="number" name="antal_personer" class="form-control" id="ap" min="1" autocomplete="off">
     	</div>
 		<input type="checkbox" id="csv_checkbox" name="csv_checkbox">
-		<label for="csv_checkbox">Få koder i CSV-format (för distansröstning)</label>
-    	<button type="submit" class="btn btn-primary" value="create" name="button">Skapa</button>
+		<label for="csv_checkbox"><?php echoLanguageChoice("Få koder i CSV-format (för distanstörstning)", "Receive codes in CSV-format (for distance voting)")?></label>
+    	<button type="submit" class="btn btn-primary" value="create" name="button"><?php echoLanguageChoice("Skapa", "Create")?></button>
     	</form>
     	</div>
 
     <?php }else{
         ?>
-        <h3>Stäng nuvarande val</h3>
+        <h3><?php echoLanguageChoice("Stäng nuvarande val", "Close current election")?></h3>
     	<hr>
     	<div class="well" style="max-width: 400px">
     		<form action="/actions/electionadminpagehandler.php" method="POST">
     			<div class="form-group">
-            <label for="psw1">Ditt lösenord:</label>
+            <label for="psw1"><?php echoLanguageChoice("Ditt lösenord:", "Your password:")?></label>
             <input type="password" name="pswuser" class="form-control" id="psw1">
     	</div>
-    	<button type="submit" class="btn btn-primary" value="delete_election" name="button">Stäng val</button>
+    	<button type="submit" class="btn btn-primary" value="delete_election" name="button"><?php echoLanguageChoice("Stäng val", "Close election")?></button>
     	</form>
     	</div>
         <?php
     }
 
 } else {
-    echo "Du har inte behörighet att visa denna sida";
+    echoLanguageChoice("Du har inte behörighet att visa denna sida.", "You don't have permission to view this page");
 }
 
  ?>

c10b135f11eb717e1b594df16a35506df993c1cf
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 15:44:11 +0200

    Finished languagePicker, needs button on frontend

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ include 'languagePicker.php';
 <html>
 
 <head>
-    <title>E-vote - Ditt digitala röstsystem</title>
+    <title><?php echo pickLanguage("E-vote - Ditt digitala röstsystem", "E-vote - Your digital voting system")?></title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
--- languagePicker.php
+++ languagePicker.php
@@ -1,6 +1,13 @@
 <?php
     /**
      * Use variable session language to return the correct string
+     * call anywhere where included (since this is in index, that is everywhere)
+     * to display the correct language
+     * 
+     * Echo response to display in html 
+     * 
+     * @param $sv Swedish text
+     * @param $en Corresponding English text
      */
     function pickLanguage($sv, $en) {
         switch ($_SESSION["lang"]) {

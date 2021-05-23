111e0e3b1286f9f89b4987b43de6a826d2875b7d
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 15:56:21 +0200

    In correct order

--- languagePicker.php
+++ languagePicker.php
@@ -1,4 +1,19 @@
 <?php
+    if (isSet($_GET["lang"])) {
+        // Set session language, so that it may be retained for 
+        // the remainder of the session
+        $_SESSION["lang"] = $_GET["lang"];
+    
+        // Save choice of language in a 365 day cookie
+        setcookie("lang", $_GET["lang"], time() + (3600 * 24 * 365));
+    }
+    else if(isSet($_COOKIE["lang"])) {
+        // Updates session according to found cookie
+        $_SESSION["lang"] = $_COOKIE["lang"];
+    }
+    else {
+        $lang = 'sv';
+    }
     /**
      * Use variable session language to return the correct string
      * call anywhere where included (since this is in index, that is everywhere)
@@ -18,20 +33,5 @@
             default:
                 return $sv;
         }
-    }
-    if (isSet($_GET["lang"])) {
-        // Set session language, so that it may be retained for 
-        // the remainder of the session
-        $_SESSION["lang"] = $_GET["lang"];
-    
-        // Save choice of language in a 365 day cookie
-        setcookie("lang", $_GET["lang"], time() + (3600 * 24 * 365));
-    }
-    else if(isSet($_COOKIE["lang"])) {
-        // Updates session according to found cookie
-        $_SESSION["lang"] = $_COOKIE["lang"];
-    }
-    else {
-        $lang = 'sv';
     }
 ?>

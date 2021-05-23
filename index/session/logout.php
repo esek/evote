39c08bbc2835a232a023e826088d2702341cfb0b
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 18:33:24 +0200

    logout fully translated

--- index/session/logout.php
+++ index/session/logout.php
@@ -1,39 +1,15 @@
 <?php
-/*
-$priv = $evote->getPrivilege($_SESSION["user"]);
-if(isset($_SESSION["sideMenu"])){
-switch($_SESSION["sideMenu"]){
-    case 'useradmin':
-        $mg->printUserhandlerPanelMenu(3);
-        break;
-    case 'adminmain':
-        $mg->printAdminMenu(3);
-        break;
-    case 'electionadmin':
-        $mg->printElectionadminPanelMenu(3);
-        break;
-    case 'adjust':
-        $mg->printAdjustPanelMenu(2);
-        break;
-}
-}
-if($_SESSION["sideMenu"] == "adminaccount"){
-
-}
-*/
-echo "<h3>Logga ut</h3>";
+echo "<h3>".pickLanguage("Logga ut", "Log out")."</h3>";
 echo "<hr>";
 echo "<div class=\"well\" style=\"max-width: 400px\">";
 echo "<div>";
-echo "Är du säker på att du vill logga ut?";
+echoLanguageChoice("Är du säker på att du vill logga ut?", "Are you sure you want to log out?");
 echo "</div>";
 echo "<br>";
 echo "<div>";
 echo "<form action=actions/usersessionhandler.php method=\"POST\">";
-echo "<button type=\"submit\" name=\"button\" value=\"logout\" class=\"btn btn-primary\" style=\"margin-bottom: 5px\">Logga ut</button>";
+echo "<button type=\"submit\" name=\"button\" value=\"logout\" class=\"btn btn-primary\" style=\"margin-bottom: 5px\">".pickLanguage("Logga ut", "Log out")."</button>";
 echo "</form>";
 echo "</div>";
 echo "</div>";
-
-
 ?>

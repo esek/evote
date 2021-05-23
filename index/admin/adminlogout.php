eedce9080dc12511da18398329146346a8212d89
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 20:50:17 +0200

    Adminlogout fully translated

--- index/admin/adminlogout.php
+++ index/admin/adminlogout.php
@@ -2,15 +2,15 @@
 
 $mg->printAdminMenu(3);
 
-echo "<h3>Logga ut som administratör</h3>";
+echo "<h3>".pickLanguage("Logga ut som administratör", "Log out as administrator")."</h3>";
 echo "<hr>";
 echo "<div style=\"max-width: 400px\">";
-echo "Är du säker på att du vill logga ut?";
+echoLanguageChoice("Är du säker på att du vill logga ut?", "Are you sure you want to log out?");
 echo "</div>";
 echo "<br>";
 echo "<div style=\"max-width: 400px\">";
 echo "<form action=actions/usersessionhandler.php method=\"POST\">";
-echo "<button type=\"submit\" name=\"button\" value=\"logout\" class=\"btn btn-primary\" style=\"margin-bottom: 5px\">Logga ut</button>";
+echo "<button type=\"submit\" name=\"button\" value=\"logout\" class=\"btn btn-primary\" style=\"margin-bottom: 5px\">".pickLanguage("Logga ut", "Log out")."</button>";
 echo "</form>";
 echo "</div>";
 

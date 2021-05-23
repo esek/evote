33612d4a696709cff376c003a1fd79bb4cda158a
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 18:03:03 +0200

    newuser.php fully translated

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ $randomString = new RandomInfo();
     </div>
 
     <!-- Main content -->
-    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main" style="min-height: 92vh">
+    <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main" style="min-height: 91vh">
 <?php
 
 
--- index/useradmin/newuser.php
+++ index/useradmin/newuser.php
@@ -10,9 +10,9 @@ if(in_array($priv, $access)){
 <div class="well" style="max-width: 400px">
     <form action="/actions/useradminpagehandler.php" method="POST">
     <div>
-            <label>Användarnamn:</label>
+            <label><?php echoLanguageChoice("Användarnamn:", "Username:")?></label>
             <input type="text" name="username" class="form-control" style="margin-bottom: 3px" autocomplete="off">
-            <label>Lösenord:</label>
+            <label><?php echoLanguageChoice("Lösenord", "Password")?></label>
             <input type="password" name="psw" class="form-control" style="margin-bottom: 3px" autocomplete="off">
             <!--
             <label>Privilegier: (0 - 2)</label>
@@ -20,34 +20,34 @@ if(in_array($priv, $access)){
         -->
     </div>
     <div class="form-group row">
-    <label class="col-sm-3 radio">Privilegier:</label>
+    <label class="col-sm-3 radio"><?php echoLanguageChoice("Privilegier:", "Privileges:")?></label>
     <div class="col-sm-9">
       <div class="radio">
         <label>
           <input type="radio" name="priv" value="1" checked>
-          Valansvarig
+          <?php echoLanguageChoice("Valansvarig", "Election admin")?>
         </label>
       </div>
       <div class="radio">
         <label>
           <input type="radio" name="priv" value="2">
-          Justerare
+          <?php echoLanguageChoice("Justerare", "Adjuster")?>
         </label>
       </div>
       <div class="radio">
         <label>
           <input type="radio" name="priv" value="0">
-          Administratör
+          <?php echoLanguageChoice("Administratör", "Administrator")?>
         </label>
       </div>
     </div>
     </div>
 
-    <button type="submit" class="btn btn-primary" value="new" name="button">Skapa ny användare</button>
+    <button type="submit" class="btn btn-primary" value="new" name="button"><?php echoLanguageChoice("Skapa ny användare", "Create new user")?></button>
     </form>
 </div>
 <?php
 } else {
-    echo "Du har inte behörighet att visa denna sida.";
+  echoLanguageChoice("Du har inte behörighet att visa denna sida.", "You don't have permission to view this page");
 }
 ?>

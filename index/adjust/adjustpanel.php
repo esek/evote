7cbc0a120f0ffa06c7399b050be1d0ca8b5e37d4
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 20:55:09 +0200

    adjustpanel fully translated

--- index/adjust/adjustpanel.php
+++ index/adjust/adjustpanel.php
@@ -10,16 +10,16 @@ if($ongoingSession){
 }
 //$mg->printAdjustPanelMenu(0);
 
-echo "<h3>Föregående valomgång</h3>";
+echo "<h3>".pickLanguage("Föregående valomgång", "Previous election round")."</h3>";
 echo "<hr>";
 
 if($ongoingSession){
 	$tg->generateResultTable("last");
 }else{
-	echo "<h4>Det pågår inget val</h4>";
+	echo "<h4>".pickLanguage("Det pågår inget val", "There is no ongoing election")."</h4>";
 }
 
 } else {
-    echo "Du har inte behörighet att visa denna sida.";
+    echoLanguageChoice("Du har inte behörighet att visa denna sida.", "You don't have permission to view this page.");
 }
 ?>

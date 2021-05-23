243011595d687c87d15097def05a393871d3c3ca
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 18:05:44 +0200

    changepassword.php fully translated

--- index/useradmin/changepassword.php
+++ index/useradmin/changepassword.php
@@ -4,21 +4,21 @@ $priv = $evote->getPrivilege($_SESSION["user"]);
 if(in_array($priv, $access)){
 //$mg->printUserhandlerPanelMenu($page);
 ?>
-<h3>Ändra lösenord</h3>
+<h3><?php echoLanguageChoice("Ändra lösenord", "Change password")?></h3>
 <hr>
 <div class="well" style="max-width: 400px">
     <form action="/actions/useradminpagehandler.php" method="POST">
     <div class="form-group">
-            <label>Användarnamn:</label>
+            <label><?php echoLanguageChoice("Änvändarnamn:", "Username:")?></label>
             <input type="text" name="username" class="form-control" style="margin-bottom: 3px" autocomplete="off">
-            <label>Nytt lösenord:</label>
+            <label><?php echoLanguageChoice("Nytt lösenord:", "New password")?></label>
             <input type="password" name="psw" class="form-control" style="margin-bottom: 3px" autocomplete="off">
     </div>
-    <button type="submit" class="btn btn-primary" value="change" name="button">Byt Lösenord</button>
+    <button type="submit" class="btn btn-primary" value="change" name="button"><?php echoLanguageChoice("Ändra lösenord", "Change password")?></button>
     </form>
 </div>
 <?php
 } else {
-    echo "Du har inte behörighet att visa denna sida.";
+    echoLanguageChoice("Du har inte behörighet att visa denna sida.", "You don't have permission to view this page");
 }
 ?>

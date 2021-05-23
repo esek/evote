10a2725750148fd9166e55f4da88aec2ba4eefd9
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-23 18:35:20 +0200

    login fully translated

--- index/session/login.php
+++ index/session/login.php
@@ -1,15 +1,15 @@
-	<h3>Logga in</h3>
+	<h3><?php echoLanguageChoice("Logga in", "Log in")?></h3>
 	<hr>
 	<div class="well" style="max-width: 400px">
 	<form action="/actions/usersessionhandler.php" method="POST">
 	<div class="form-group">
-  		<label for="usr">Namn:</label>
+  		<label for="usr"><?php echoLanguageChoice("Namn:", "Name:")?></label>
  		<input type="text" class="form-control" name="usr" autocomplete="off">
 	</div>
 	<div class="form-group">
-  		<label for="pwd">Lösenord:</label>
+  		<label for="pwd"><?php echoLanguageChoice("Lösenord:", "Password:")?></label>
   		<input type="password" class="form-control" name="psw">
 	</div>
-	<button type="submit" class="btn btn-primary" name="button" value="login" name="login">Logga in</button>
+	<button type="submit" class="btn btn-primary" name="button" value="login" name="login"><?php echoLanguageChoice("Logga in", "Log in")?></button>
 	</form>
 	</div>

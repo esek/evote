2e1bf593bcae0df6eb9afd427a1beacee674a7b6
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-25 20:30:12 +0200

    Remove old commented code

--- index/electionadmin/electionadminpanel.php
+++ index/electionadmin/electionadminpanel.php
@@ -19,23 +19,6 @@ if($evote->checkCheating()){
 
 if(!$ongoingSession){ ?>
 	<h4>Det pågår inget valtillfälle.</h4>
-<!--
-	<h3>Skapa nytt val</h3>
-	<hr>
-	<div style="max-width: 400px">
-	<form action="actions/electionadminpagehandler.php" method="POST">
-	<div class="form-group">
-	        <label for="vn">Namn på val:</label>
-	        <input type="text" name="valnamn" class="form-control" id="vn" autocomplete="off">
-	</div>
-	<div class="form-group" style="max-width: 150px">
-	        <label for="ap">Max antal personer:</label>
-	        <input type="number" name="antal_personer" class="form-control" id="ap" min="1" autocomplete="off">
-	</div>
-	<button type="submit" class="btn btn-primary" value="create" name="button">Skapa</button>
-	</form>
-	</div>
--->
 <?php }else{
 	$ongoing = $evote->ongoingRound();
 	# ---------------NY VALOMGÅNG OCH VISA FÖRRA VALOMGÅNGEN --------------

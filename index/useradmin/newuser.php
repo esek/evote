6491115c2a2b509a0b8def4e0e482b6f9f2968cd
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-05-25 20:35:55 +0200

    Removes commented PHP

--- index/useradmin/newuser.php
+++ index/useradmin/newuser.php
@@ -1,10 +1,7 @@
 <?php
 $access = array(0);
 $priv = $evote->getPrivilege($_SESSION["user"]);
-if(in_array($priv, $access)){
-//$mg->printUserhandlerPanelMenu($page);
-?>
-
+if(in_array($priv, $access)){?>
 <h3>Skapa ny användare</h3>
 <hr>
 <div class="well" style="max-width: 400px">
@@ -14,10 +11,6 @@ if(in_array($priv, $access)){
             <input type="text" name="username" class="form-control" style="margin-bottom: 3px" autocomplete="off">
             <label>Lösenord:</label>
             <input type="password" name="psw" class="form-control" style="margin-bottom: 3px" autocomplete="off">
-            <!--
-            <label>Privilegier: (0 - 2)</label>
-            <input type="number" name="priv" class="form-control" min="0" max="2" style="margin-bottom: 3px; max-width: 200px" autocomplete="off">
-        -->
     </div>
     <div class="form-group row">
     <label class="col-sm-3 radio">Privilegier:</label>

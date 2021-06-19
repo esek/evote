97b5385a96de4292d45c0da8f1522183c699a760
Emil Jonathan Eriksson <eje1999@gmail.com>
2021-06-19 13:07:08 +0200

    Fix indentation

--- index/admin/electionControl.php
+++ index/admin/electionControl.php
@@ -20,17 +20,17 @@ if(in_array($priv, $access)){
     	<div class="well" style="max-width: 400px">
 			<div id="new-election-form">
 				<form action="/actions/adminpagehandler.php" method="POST">
-				<div class="form-group">
-						<label for="vn">Namn på val:</label>
-						<input type="text" name="valnamn" class="form-control" id="vn" autocomplete="off">
-				</div>
-				<div class="form-group" style="max-width: 150px">
-						<label for="ap">Max antal personer:</label>
-						<input type="number" name="antal_personer" class="form-control" id="ap" min="1" autocomplete="off">
-				</div>
-				<input type="checkbox" id="csv_checkbox" name="csv_checkbox">
-				<label for="csv_checkbox">Få koder i CSV-format (för distansröstning)</label>
-				<button type="submit" onclick="placeholder()" class="btn btn-primary" value="create" name="button">Skapa</button>
+					<div class="form-group">
+							<label for="vn">Namn på val:</label>
+							<input type="text" name="valnamn" class="form-control" id="vn" autocomplete="off">
+					</div>
+					<div class="form-group" style="max-width: 150px">
+							<label for="ap">Max antal personer:</label>
+							<input type="number" name="antal_personer" class="form-control" id="ap" min="1" autocomplete="off">
+					</div>
+					<input type="checkbox" id="csv_checkbox" name="csv_checkbox">
+					<label for="csv_checkbox">Få koder i CSV-format (för distansröstning)</label>
+					<button type="submit" onclick="placeholder()" class="btn btn-primary" value="create" name="button">Skapa</button>
 				</form>
 			</div>
 			<div id="submit-placeholder" style="display: none">
@@ -55,11 +55,11 @@ if(in_array($priv, $access)){
     	<div class="well" style="max-width: 400px">
     		<form action="/actions/adminpagehandler.php" method="POST">
     			<div class="form-group">
-            <label for="psw1">Ditt lösenord:</label>
-            <input type="password" name="pswuser" class="form-control" id="psw1">
-    	</div>
-    	<button type="submit" class="btn btn-primary" value="delete_election" name="button">Stäng val</button>
-    	</form>
+            		<label for="psw1">Ditt lösenord:</label>
+            		<input type="password" name="pswuser" class="form-control" id="psw1">
+    			</div>
+    			<button type="submit" class="btn btn-primary" value="delete_election" name="button">Stäng val</button>
+    		</form>
     	</div>
         <?php
     }
